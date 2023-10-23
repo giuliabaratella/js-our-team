3ccd7d00ae2fce6a324f4991520261041f536962
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-23 15:18:21 +0200

    added team array + entered info about menbers + console.log

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+
+// Name            Role                    Picture
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+const team = [
+    {
+        name: 'Wayne',
+        surname: 'Barnett',
+        role: 'Founder & CEO',
+        picture: 'wayne-barnett-founder-ceo.jpg' 
+    },
+    {
+        name: 'Angela',
+        surname: 'Carroll',
+        role: 'Chief Editor',
+        picture: 'angela-caroll-chief-editor.jpg' 
+    },
+    {
+        name: 'Walter',
+        surname: 'Gordon',
+        role: 'Office Manager',
+        picture: 'walter-gordon-office-manager.jpg' 
+    },
+    {
+        name: 'Angela',
+        surname: 'Lopez',
+        role: 'Social Media Manager',
+        picture: 'angela-lopez-social-media-manager.jpg' 
+    },
+    {
+        name: 'Scott',
+        surname: 'Estrada',
+        role: 'Developer',
+        picture: 'scott-estrada-developer.jpg' 
+    },
+    {
+        name: 'Barbara',
+        surname: 'Ramos',
+        role: 'Graphic Designer',
+        picture: 'barbara-ramos-graphic-designer.jpg' 
+    }
+
+];
+console.log (team);
